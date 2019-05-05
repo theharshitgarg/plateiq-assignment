f27e603412a3baddf85a26f1ded342c20a84f24d
Harshit Garg <harshitgargmnit@gmail.com>
2019-05-05 10:14:33 +0530

    Add update digitized document feature

--- docs_management/urls.py
+++ docs_management/urls.py
@@ -8,10 +8,9 @@ urlpatterns = [
     url(r'list/(\d+)/update-status',
         views.update_status_view,
         name='update_status'),
-    url(r'list/(\d+)/digitized', views.digitized_doc_details, name='list_docs'),
+    url(r'list/digitized/(\d+)', views.digitized_doc_details, name='list_digitized_docs'),
     url(r'upload-doc', views.upload_doc, name='upload_doc'),
     url(r'list', views.list_uploaded_docs, name='list_docs'),
-
 
     # API Views
     url(r'api/v1/list',
